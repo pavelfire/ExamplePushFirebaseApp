111013b63f528218a5ce5a30b3c54ff08004b7d7
PVm116rus <fire_ra@bk.ru>
2024-06-06 09:11:33 +0300

    icon notification changed

--- app/src/main/java/com/vk/directop/examplepushfirebaseapp/ChatViewModel.kt
+++ app/src/main/java/com/vk/directop/examplepushfirebaseapp/ChatViewModel.kt
@@ -5,7 +5,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.google.firebase.Firebase
+import com.google.firebase.messaging.messaging
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import okio.IOException
 import retrofit2.HttpException
 import retrofit2.Retrofit
@@ -24,6 +27,12 @@ class ChatViewModel : ViewModel() {
         .addConverterFactory(MoshiConverterFactory.create())
         .build()
         .create()
+
+    init {
+        viewModelScope.launch {
+            Firebase.messaging.subscribeToTopic("chat").await()
+        }
+    }
 
     fun onRemoteTokenChange(newToken: String) {
         state = state.copy(
